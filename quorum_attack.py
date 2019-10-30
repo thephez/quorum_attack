f3ea70e2a621d4d7b9fbce41b7312afbcf4e95a8
thephez <thephez@users.noreply.github.com>
2019-10-30 13:18:16 -0400

    Convert to Python3 style Include original binom function and check against new one

--- quorum_attack.py
+++ quorum_attack.py
@@ -3,6 +3,17 @@ from decimal import Decimal
 from math import log
 from math import factorial as fac
 
+def binom_orig(n, m):
+    b = [0] * (n + 1)
+    b[0] = 1
+    for i in range(1, n + 1):
+        b[i] = 1
+        j = i - 1
+        while j > 0:
+            b[j] += b[j - 1]
+            j -= 1
+    return b[m]
+
 def binom(x, y):
     try:
         binom = fac(x) // fac(y) // fac(x - y)
@@ -10,52 +21,39 @@ def binom(x, y):
         binom = 0
     return binom
 
-#Totoal number of masternodes
+#Total number of masternodes
 mns=5000
-#Quarum size used for ChainLocks
-qsz=400
-#Number of nodes in a LLMQ needed for a CHainLock
+#Quorum size used for ChainLocks
+qsz=500
+#Number of nodes in a LLMQ needed for a ChainLock
 qmaj=240
 
-print "Assume ", mns," masternodes in total"
+print("Assume {} masternodes in total".format(mns))
+print("The number of masternodes in a quorum: {}".format(qsz))
 
-print "The number of masternodes in a quourm:  ", qsz
-
-numb = binom(mns,qsz)
-print "Total number of LLMQs:" 
-
-print numb
+numb = binom(mns, qsz)
+print("Total number of LLMQs: {}".format(numb))
 
 #Number of attacking nodes
-y = 500
+y = 400
 
-print "Assume ", y, " of MNs are Byzintine"
-
+print("Assume {} of MNs are Byzantine".format(y))
 temp = 0
+temp2 = 0
 
-for x in range(qmaj,qsz+1):
-    print "Number of LLMQs with ", x," Byzinetine nodes:"
-    temp = temp + binom(y,x)*binom(mns-y,qsz -x)
-    print binom(y,x)*binom(mns-y,qsz-x)
-    #line below was in here for trouble shooting
-    #print temp
+for x in range(qmaj, qsz+1):
+    print("\nNumber of LLMQs with {} Byzantine nodes:".format(x))
+    temp = temp + binom(y, x) * binom(mns-y, qsz -x)
+    temp2 = temp2 + binom_orig(y, x) * binom_orig(mns-y, qsz -x)
+    if temp != temp2:
+        print("!!!!!!!!!!!!!!!! ERRROR !!!!!!!!!!!!!!!!!!!!!!!!")
+        print("\n{}\n{}\n".format(temp, temp2))
+    #print("\tB: {}".format(binom(y, x) * binom(mns-y, qsz-x)))
+    #print("\n\ttemp: {}".format(temp))
 
-
-print "Total number of Byzinetine Quorums" 
-print temp
-
-print "Log base 10 of above:"
-
-print log(temp,10)
-
-print "Total number of LLMQs"
-print numb
-
-print "Log base 10 of above:"
-
-print log(numb,10)
-
-print "Probabilty of malious ChainLock" 
-
-print 10 ** (log(temp,10)-log(numb,10))
+print("Total number of Byzantine Quorums: {}".format(temp))
+print("Log base 10 of above: {}".format(log(temp, 10)))
+print("Total number of LLMQs: {}".format(numb))
+print("Log base 10 of above: {}".format(log(numb, 10)))
+print("Probabilty of malicious ChainLock: {}".format(10 ** (log(temp, 10)-log(numb, 10))))
 
